4c38fc3b675de2c9995c850a1ac928469ecbf521
Chirag Greed <chiraggreed02@gmail.com>
2026-08-16 12:49:38 +0530

    Add initial HTML structure, CSS styles, and GSAP animations for landing page

---
+++ script.js
@@ -0,0 +1,31 @@
+var tl = gsap.timeline();
+tl.to("#something1",{
+    x:1000,
+    y:200,
+    borderRadius:10,
+    backgroundColor:"rgba(200, 127, 0, 1)",
+    rotate:360,
+    duration:1,
+    delay:2,
+});
+
+tl.to("#something2",{
+    x:1000,
+    y:200,
+    borderRadius:10,
+    backgroundColor:"rgba(41, 114, 91, 1)",
+    rotate:360,
+    duration:1,
+});
+
+tl.to("#something3",{
+    x:1000,
+    y:200,
+    borderRadius:10,
+    backgroundColor:"rgba(0, 200, 133, 1)",
+    rotate:360,
+    duration:1,
+});
+
+
+
